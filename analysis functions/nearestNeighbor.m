8ba8eb31a1c1560d6695441e0a24c235d22d3825
Zachary Daniels <zad309@lehigh.edu>
2012-11-01 20:50:15 -0500

    Nearest neighbor completed

--- analysis functions/nearestNeighbor.m
+++ analysis functions/nearestNeighbor.m
@@ -1,13 +1,19 @@
-function f = nearestNeighbor3D(A,B,index)
-Bx = B(index,1);
-By = B(index,2);
-Bz = B(index,3);
-BxExpanded = repmat(Bx,rows(A),1);
-ByExpanded = repmat(By,rows(A),1);
-BzExpanded = repmat(Bz,rows(A),1);
-C = [A(:,1) - BxExpanded, A(:,2) - ByExpanded, A(:,3) - BzExpanded];
-C = C .* C;
-D = C(:,1) + C(:,2) + C(:,3);
-D = sqrt(D);
-minLoc = find(D(:,1) == min(D(:,1)));
-f = A(minLoc,4);
+function f = nearestNeighbor(means,data)
+
+numFeatures = columns(means) - 1;
+dataManipulation = means(:,1:numFeatures);
+
+for j = 1:1:numFeatures
+	dataManipulation(:,j) = dataManipulation(:,j) - data(1,j);
+endfor
+
+dataManipulation = dataManipulation .^ 2;
+
+dataManipulation = dataManipulation';
+
+dataManipulation = sum(dataManipulation)';
+
+minLoc = find(dataManipulation(:,1) == min(dataManipulation(:,1)));
+
+f = means(minLoc,numFeatures + 1);
+endfunction
